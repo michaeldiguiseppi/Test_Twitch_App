166d590a68967568751bc72ef9b965e55ba52692
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2017-02-03 20:57:14 -0700

    Initial commit

---
+++ src/server/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/test', function(req, res, next) {
+  res.json({ message: "The test was successful!", status: "Success" })
+});
+
+module.exports = router;
